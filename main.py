6f5c6f7cc9591ee6e9f29f048b5b85bcc960e544
and-huang <106034721+and-huang@users.noreply.github.com>
2023-10-01 18:18:05 -0400

    Update main.py
    You will only receive an email if the price is lower than the previously recorded lowest price. The lowest historical price is recorded in a .txt file.

--- main.py
+++ main.py
@@ -15,21 +15,30 @@ price = soup.find("span", attrs={"class": "psw-t-title-m psw-m-r-4"})
 time.sleep(random.randint(2, 6))
 title = soup.find("h1", attrs={"class": "game-title"})
 
-port = 000 #Replace with port
-smtp_server = "" #I used smtp.gmail.com
-sender_email = "" #Replace with your email
-receiver_email = "" #Replace with recipient email
-password = "" #For Gmail users, you have to enable 2FA and request an app password in settings, separate from your normal Gmail password
-message = f"""\
-Subject: Price of PS4 game
+path = "replace this with the absolute path of your .txt file for storing the lowest price"
 
-As of {date.today()}, the price of {title.text} is {price.text}."""
+lowestPrice = open(path, "r")
+currLowest = lowestPrice.read()
+currLowestFloat = float(currLowest[1:6])
+if float(price.text[1:6]) < currLowestFloat: #You only get emailed if the price is lower
+    newLowest = open(path, "w")
+    newLowest.write(price.text)
 
-#Establish client-server encryption
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-
+    port = 000 #Replace with port
+    smtp_server = "" #I used smtp.gmail.com
+    sender_email = "" #Replace with your email
+    receiver_email = "" #Replace with recipient email
+    password = "" #For Gmail users, you have to enable 2FA and request an app password in settings, separate from your normal Gmail password
+    message = f"""\
+    Subject: Price of PS4 game
+    
+    As of {date.today()}, the price of {title.text} is {price.text}."""
+    
+    #Establish client-server encryption
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+newLowest.close()
 #For Windows users, you can automate/schedule this file using Task Scheduler
 #For Mac users, you can automate/schedule this file using Apple's Automator
