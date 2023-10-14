c15894ed93d4155ae039ad787bf61a42baf99a9a
and-huang <106034721+and-huang@users.noreply.github.com>
2023-10-14 11:33:00 -0400

    Bug fixes
    Fixed cases where the price is over $99 or the game is not on sale.

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ URL = "https://www.playstation.com/en-us/games/insert-title-of-game-here/" #Modi
 response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, "html.parser")
-time.sleep(random.randint(2, 6)) #This makes it slightly less likely that we will be detected by some security protocols
-price = soup.find("span", attrs={"class": "psw-t-title-m psw-m-r-4"})
+time.sleep(random.randint(2, 6))
+if soup.find("span", attrs={"class": "psw-t-title-m psw-m-r-4"}) == None:
+	price = soup.find("span", attrs={"class": "psw-t-title-m"})
+else:
+	price = soup.find("span", attrs={"class": "psw-t-title-m psw-m-r-4"})
 time.sleep(random.randint(2, 6))
 title = soup.find("h1", attrs={"class": "game-title"})
 
-path = "replace this with the absolute path of your .txt file for storing the lowest price"
+path = "" #replace with absolute path to your text file storing the lowest price
 
 lowestPrice = open(path, "r")
 currLowest = lowestPrice.read()
-currLowestFloat = float(currLowest[1:6])
-if float(price.text[1:6]) < currLowestFloat: #You only get emailed if the price is lower
+currLowestFloat = float(currLowest[1:7])
+if float(price.text[1:7]) < currLowestFloat: #You only get emailed if the price is lower
     newLowest = open(path, "w")
     newLowest.write(price.text)
 
@@ -30,7 +33,7 @@ if float(price.text[1:6]) < currLowestFloat: #You only get emailed if the price 
     receiver_email = "" #Replace with recipient email
     password = "" #For Gmail users, you have to enable 2FA and request an app password in settings, separate from your normal Gmail password
     message = f"""\
-    Subject: Price of PS4 game
+    Subject: Price of {title.text}
     
     As of {date.today()}, the price of {title.text} is {price.text}."""
     
@@ -39,6 +42,7 @@ if float(price.text[1:6]) < currLowestFloat: #You only get emailed if the price 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
-newLowest.close()
+    newLowest.close()
+lowestPrice.close()
 #For Windows users, you can automate/schedule this file using Task Scheduler
 #For Mac users, you can automate/schedule this file using Apple's Automator
